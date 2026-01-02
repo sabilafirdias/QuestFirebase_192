143eba04371f3e73f773f5b4bc910b78a118553e
sabilafirdias <sabilafirdias@gmail.com>
2026-01-02 18:59:27 +0700

    membuat  file DestinasiDetail.kt

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiDetail.kt
@@ -3,9 +3,3 @@ package com.example.myfirebase.view.route
 import com.example.myfirebase.R
 import com.example.myfirebase.view.route.DestinasiNavigasi
 
-object DestinasiDetail: DestinasiNavigasi {
-    override val route = "detail_siswa"
-    override val titleRes = R.string.detail_siswa
-    const val itemIdArg = "idSiswa"
-    val routeWithArgs = "$route/{$itemIdArg}"
-}
