cfbdb35c27b6edc8c6f384c8e7c8551a72d7d607
sabilafirdias <sabilafirdias@gmail.com>
2026-01-02 18:57:58 +0700

    membuat file DestinasiHome.kt

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiHome.kt
@@ -3,7 +3,3 @@ package com.example.myfirebase.view.route
 import com.example.myfirebase.R
 import com.example.myfirebase.view.route.DestinasiNavigasi
 
-object DestinasiHome : DestinasiNavigasi {
-    override val route = "home"
-    override val titleRes = R.string.app_name
-}
