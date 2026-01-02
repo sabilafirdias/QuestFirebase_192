1ae725f0b4b72b77706b9de6ffcbde208967b77c
sabilafirdias <sabilafirdias@gmail.com>
2026-01-02 18:59:14 +0700

    membuat isi file DestinasiEdit.kt

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
@@ -3,3 +3,9 @@ package com.example.myfirebase.view.route
 import com.example.myfirebase.R
 import com.example.myfirebase.view.route.DestinasiNavigasi
 
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
