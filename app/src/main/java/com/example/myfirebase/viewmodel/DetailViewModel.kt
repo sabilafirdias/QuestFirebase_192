4ac3782a98c5f312fb104f54ae0f620f7f885b4c
sabilafirdias <sabilafirdias@gmail.com>
2026-01-09 18:15:09 +0700

    membuat sealed interface statusuidetail

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+}
+
