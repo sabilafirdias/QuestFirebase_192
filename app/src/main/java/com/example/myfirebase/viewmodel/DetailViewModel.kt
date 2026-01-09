70aaaae032bf2be4da4e96bf3cc9bd6787686346
sabilafirdias <sabilafirdias@gmail.com>
2026-01-09 18:17:21 +0700

    membuat suspend function hapusSatuSiswa

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -41,5 +41,12 @@ class DetailViewModel(savedStateHandle: SavedStateHandle, private val repository
         }
     }
 
-
+    suspend fun hapusSatuSiswa() {
+        try {
+            repositorySiswa.hapusSatuSiswa(idSiswa)
+            println("Sukses Hapus Data: $idSiswa")
+        } catch (e: Exception) {
+            println("Gagal Hapus Data: ${e.message}")
+        }
+    }
 }
